6e62cd82878bfb2533610ae7a735096cf5d1460b
I <bennigraf@Schabbock.local>
2017-07-15 22:49:18 +0200

    added message to run file

--- app/run-with-docker.sh
+++ app/run-with-docker.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Open http://localhost:8090 to view app after app boot."
+
 # docker run -d -p 80 -v `pwd`:/www fnichol/uhttpd
 docker run --rm -p 8090:80 -v $(pwd)/www:/www fnichol/uhttpd
