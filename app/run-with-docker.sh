30f6f92b100a46c88d8a62b4096f75ceff051d3d
Benjamin Graf <bennigraf@gmail.com>
2017-10-13 23:22:35 +0200

    change directory structure and prepare for webpack

--- app/run-with-docker.sh
+++ app/run-with-docker.sh
@@ -3,4 +3,4 @@
 echo "Open http://localhost:8090 to view app after app boot."
 
 # docker run -d -p 80 -v `pwd`:/www fnichol/uhttpd
-docker run --rm -p 8090:80 -v $(pwd)/www:/www fnichol/uhttpd
+docker run --rm -p 8090:80 -v $(pwd)/dist:/www fnichol/uhttpd
