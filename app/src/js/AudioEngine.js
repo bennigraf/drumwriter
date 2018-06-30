d7948a32b8014be3a5a8e47200a84f86e405c824
Benjamin Graf <bennigraf@gmail.com>
2018-06-30 15:15:24 +0200

    Use Tone.Players as sample player in AudioEngine

--- app/src/js/AudioEngine.js
+++ app/src/js/AudioEngine.js
@@ -1,35 +1,24 @@
 
 function AudioEngine() {
-  this.bass = new Tone.Player('e.mp3').toMaster();
-  this.snare = new Tone.Player('k.mp3').toMaster();
+  this.samplePlayers = new Tone.Players({
+    b: 'e.mp3', // bassdrum
+    t: 'k.mp3', // snare
+    z: 'h.mp3' // HiHat
+  }).toMaster();
 }
 module.exports = AudioEngine;
 
 AudioEngine.prototype = {
   
-  playInstrument: function(instrument) {
-    console.log('playing ' + instrument);
-    switch (instrument) {
-    case 'b':
-      this.bass.start();
-      break;
-    case 't':
-      this.snare.start();
+  playInstrument: function(instrument, volume = -6) {
+    console.log('playing in AudioEngine: ' + instrument, volume);
+
+    try {
+      this.samplePlayers.get(instrument).volume.value = volume;
+      this.samplePlayers.get(instrument).start();
+    } catch (e) {
+      console.info('no player found for ' + instrument);
     }
+    
   }
-  
-// play: function() {
-//
-//     this.bassBeat = Tone.Transport.scheduleRepeat(() => {
-//         console.log("bass!");
-//         this.bass.start();
-//     }, 1);
-//     console.log(this.bassBeat);
-//   },
-//
-//   stop: function() {
-//       console.log("stop");
-//       Tone.Transport.clear(this.bassBeat);
-//       Tone.Transport.stop();
-//   }
 }
